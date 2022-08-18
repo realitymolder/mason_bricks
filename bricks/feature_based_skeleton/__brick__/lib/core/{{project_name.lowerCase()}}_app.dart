159551484d99734a5925994223565d0898d4df6a
Daniel Toubul <molder@toubul.eu>
2022-08-18 17:19:07 +0300

    :fire: core and app files where named wrongly

--- bricks/feature_based_skeleton/__brick__/lib/core/{{project_name.lowerCase()}}_app.dart
+++ bricks/feature_based_skeleton/__brick__/lib/core/{{project_name.lowerCase()}}_app.dart
@@ -1,18 +1,12 @@
-import 'package:{{package_name}}/core/{{project_name.lowerCase()}}_app.dart';
-import 'package:{{package_name}}/theme/{{project_name.lowerCase()}}_theme.dart';
 import 'package:flutter/material.dart';
 
-class {{project_name}}Core extends StatelessWidget {
-  const {{project_name}}Core({Key? key}) : super(key: key);
+class {{project_name}}App extends StatelessWidget {
+  const {{project_name}}App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This widget is the root of your application.
-    return MaterialApp(
-      themeMode: ThemeMode.light,
-      theme: {{project_name}}Theme.light(),
-      darkTheme: {{project_name}}Theme.dark(),
-      home: const {{project_name}}App(),
+    return const Scaffold(
+      body: FirstPage(),
     );
   }
 }
--- bricks/feature_based_skeleton/__brick__/lib/main.dart
+++ bricks/feature_based_skeleton/__brick__/lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:{{package_name}}/core/{{project_name.lowerCase()}}_core.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
 
- runApp(
+void main() {
+runApp(
     const ProviderScope(
       child: {{project_name}}Core(),
     ),
   );
+}
